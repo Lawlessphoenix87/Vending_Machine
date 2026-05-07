76786a7721c4b5aef9753de4c4d03bd95d91cc8a
Lawlessphoenix87 <87556244+Lawlessphoenix87@users.noreply.github.com>
2026-05-06 23:20:35 -0700

    Update VendingMachine.java
    Defined private fields, the constructor method,  and a stock item method

--- VendingMachine/src/VendingMachine.java
+++ VendingMachine/src/VendingMachine.java
@@ -4,5 +4,33 @@
 
 public class VendingMachine 
 {
+    private String[] _itemNames;
+    private double[] _itemPrices;
+    private int[] _itemQuantities;
+    private int _itemCount;
+    private int _maxItems;
 
+    public VendingMachine(int maxItems)
+    {
+        _maxItems = maxItems;
+
+        _itemNames = new String[maxItems];
+        _itemPrices = new double[maxItems];
+        _itemQuantities = new int[maxItems];
+        _itemCount = 0;
+    }
+
+    public void StockItem(String name, double price, int quantity)
+    {
+        if(_itemCount >= _maxItems)
+            System.out.println("Machine is full. Cannot add more items.");
+        else
+        {
+            _itemNames[_itemCount] = name;
+            _itemPrices[_itemCount] = price;
+            _itemQuantities[_itemCount] = quantity;
+            _itemCount++;
+            System.out.printf("Stocked: %s ($%.2f) x %dn", name, price, quantity);
+        }
+    }
 }
